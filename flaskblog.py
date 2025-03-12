3a3b16bcb39a165133e2160136fcdd29e1cf5f8e
nikhilss219 <nikhil.wilsonk@gmail.com>
2025-03-12 17:17:11 +0530

    added database class

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from flask import Flask,render_template,url_for,flash,redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
 
 '''
@@ -8,9 +10,34 @@ flask run
 export FLASK_DEBUG=1
 flask run now runs in debug mode
 '''
+
 app = Flask(__name__)##__name__ means name of the module; to look for static file and templates
 
 app.config['SECRET_KEY']='85c597fcbad01a63ac800b858ac97f27'
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'#/// is for relative path, here the project path itself
+
+db=SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(20),unique=True,nullable=False)
+    email=db.Column(db.String(120),unique=True,nullable=False)
+    image_file=db.Column(db.String(20),nullable=False,default='default.jpg')
+    password=db.Column(db.String(60),nullable=False)
+    posts=db.relationship('Post',backref='author',lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}','{self.image_file}')"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(100),nullable=False)
+    date_posted=db.Column(db.DateTime(100),nullable=False,default=datetime.utcnow)
+    content=db.Column(db.Text, nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
 
 posts=[
     {
@@ -26,6 +53,7 @@ posts=[
         'date_posted':'10/03/25'
     },
 ]
+
 @app.route("/")
 @app.route("/home")#to add one more route to this page
 def home():
@@ -67,8 +95,7 @@ def login_page():
 this condition is only when run with python directly here
 if we import it from somewhere else, this will not run
 '''
-
 if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True) #now will debug
-
-
